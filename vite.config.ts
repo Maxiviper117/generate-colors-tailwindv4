0893e44700ef0d62e597ac6852ec7e8c562ab5ac
David <davidgonsal@gmail.com>
2025-03-07 11:33:36 +0200

    feat: enhance Vite configuration with environment loading and production optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,31 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
-	
+// export default defineConfig({
+// 	plugins: [sveltekit(), tailwindcss()]
+// });
+
+// "dependencies": {
+// 		"@types/chroma-js": "^3.1.1",
+// 		"chroma-js": "^3.1.2",
+// 		"lucide-svelte": "^0.475.0"
+// 	}
+
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	console.log({ command, mode });
+
+	const isProduction = mode === 'production';
+	return {
+		plugins: [sveltekit(), tailwindcss()],
+		optimizeDeps: {
+			force: isProduction,
+			include: [
+				'chroma-js',
+				'lucide-svelte',
+			]
+		}
+	};
 });
